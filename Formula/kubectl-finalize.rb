20c52a48e1bee480f9145bb39ccc9ffc1cd501a4
Teo <teocns@gmail.com>
2025-01-06 22:55:39 +0100

    fix: update formula installation

--- Formula/kubectl-finalize.rb
+++ Formula/kubectl-finalize.rb
@@ -7,19 +7,30 @@ class KubectlFinalize < Formula
     if Hardware::CPU.arm?
       url "https://github.com/teocns/kubectl-finalize/releases/download/v0.1.1/kubectl-finalize-darwin-arm64"
       sha256 "454964eee7a50ef3c125b272fc8da2b74dcdd2a1438e7bbf5229b0d898e27b7b"
+
+      def install
+        binary_name = "kubectl-finalize"
+        bin.install "kubectl-finalize-darwin-arm64" => binary_name
+      end
     else
       url "https://github.com/teocns/kubectl-finalize/releases/download/v0.1.1/kubectl-finalize-darwin-amd64"
       sha256 "0363fad51d615557902e5d735d09a523539b9cba1897fefcca6298f6eb983d49"
+
+      def install
+        binary_name = "kubectl-finalize"
+        bin.install "kubectl-finalize-darwin-amd64" => binary_name
+      end
     end
   end
 
   on_linux do
     url "https://github.com/teocns/kubectl-finalize/releases/download/v0.1.1/kubectl-finalize-linux-amd64"
     sha256 "773bc2b86b4b621738789563c27ac20d4ba765d19d9ec623cf480c4c6783344f"
-  end
 
-  def install
-    bin.install Dir["kubectl-finalize-*"].first => "kubectl-finalize"
+    def install
+      binary_name = "kubectl-finalize"
+      bin.install "kubectl-finalize-linux-amd64" => binary_name
+    end
   end
 
   test do
